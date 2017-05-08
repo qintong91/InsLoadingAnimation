c8b871fd773f58e56b9b72813993e6af1af421cd
qintong <qintong5900@163.com>
2017-05-09 00:05:48 +0800

    fit circle animation, adjust code

--- library/src/main/java/com/example/qintong/library/InsLoadingView.java
+++ library/src/main/java/com/example/qintong/library/InsLoadingView.java
@@ -7,20 +7,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.graphics.drawable.Animatable;
-import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.AnticipateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 
 import java.util.ArrayList;
 
 public class InsLoadingView  extends View {
+    private static String TAG = "InsLoadingView";
+    private static boolean DEBUG = true;
 
     private float degress;
     private float arcWidth;
@@ -52,40 +50,53 @@ public class InsLoadingView  extends View {
     @Override
     protected synchronized void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Log.d("qintong1", "onDraw " + getWidth() + "---" + getHeight());
-        //drawTrack(canvas);
-        drawxxx(canvas, new Paint());
+        if (DEBUG)  {
+            Log.d(TAG, "onDraw " + getWidth() + "---" + getHeight());
+        }
+        drawTrack(canvas, new Paint());
         postInvalidate();
     }
 
-    void drawxxx(Canvas canvas, Paint paint) {
+    void drawTrack(Canvas canvas, Paint paint) {
         paint.setColor(Color.BLACK);
         canvas.rotate(degress,centerX(),centerY());
         //canvas.drawArc(rectF,-60,30,false,paint);
         canvas.rotate(12,centerX(),centerY());
-        RectF rectF=new RectF((float) (getWidth()*0.1),(float) (getWidth()*0.1),(float) (getWidth()*0.9), (float)(getHeight()*0.9));
+        RectF rectF=new RectF((float) (getWidth()*0.2),(float) (getWidth()*0.2),(float) (getWidth()*0.8), (float)(getHeight()*0.8));
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth((float)(getHeight()*0.02));
         //canvas.drawArc(rectF,0,cricleWidth,false,paint);
-        Log.d("qintong", "cricleWidth:" + cricleWidth);
-        for (int i = 0; i <= 4 ; i ++) {
-            if (12*i > cricleWidth) {
-                break;
-            }
-            canvas.drawArc(rectF,cricleWidth - 12*i,8 + i,false,paint);
+        if (DEBUG)  {
+            Log.d(TAG, "cricleWidth:" + cricleWidth);
         }
-        if (cricleWidth > 48 ) {
-            canvas.drawArc(rectF,0,cricleWidth - 48,false,paint);
-        }
-
         if (cricleWidth < 0) {
             canvas.drawArc(rectF,cricleWidth,0-cricleWidth,false,paint);
             float adjustCricleWidth = cricleWidth + 360;
-            float width = 12;
+            float width = 8;
             while (adjustCricleWidth > 0) {
-                width = width - 0.4f;
+                width = width - 0.2f;
                 adjustCricleWidth = adjustCricleWidth -12;
                 canvas.drawArc(rectF,adjustCricleWidth ,width ,false,paint);
+            }
+        } else {
+            for (int i = 0; i <= 4 ; i ++) {
+                if (12*i > cricleWidth) {
+                    break;
+                }
+                canvas.drawArc(rectF,cricleWidth - 12*i,8 + i,false,paint);
+            }
+            if (cricleWidth > 48 ) {
+                canvas.drawArc(rectF,0,cricleWidth - 48,false,paint);
+            }
+            float adjustCricleWidth = 360;
+            float width = 8 * (360 - cricleWidth)/360;
+            if (DEBUG)  {
+                Log.d(TAG, "width:" + width);
+            }
+            while (width > 0) {
+                width = width - 0.2f;
+                adjustCricleWidth = adjustCricleWidth  - 12;
+                canvas.drawArc(rectF, adjustCricleWidth ,width ,false,paint);
             }
         }
 
@@ -121,7 +132,7 @@ public class InsLoadingView  extends View {
         rotateAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                //degress= (float) animation.getAnimatedValue();
+                degress= (float) animation.getAnimatedValue();
                 postInvalidate();
             }
         });
