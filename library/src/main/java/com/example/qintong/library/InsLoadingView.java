d6f8633eae14ba635c463d9dd0a9e2a0aeb425e3
qintong1 <qintong1@xiaomi.com>
2017-05-09 21:04:23 +0800

    adjust interpolator add paint color

--- library/src/main/java/com/example/qintong/library/InsLoadingView.java
+++ library/src/main/java/com/example/qintong/library/InsLoadingView.java
@@ -1,28 +1,35 @@
 package com.example.qintong.library;
 
+import android.animation.Animator;
 import android.animation.ValueAnimator;
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.os.Build;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.Interpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 
 import java.util.ArrayList;
 
-public class InsLoadingView  extends View {
+import static android.graphics.Shader.TileMode.CLAMP;
+
+public class InsLoadingView extends View {
     private static String TAG = "InsLoadingView";
     private static boolean DEBUG = true;
+    private double circleDia = 0.9;
+    private float strokeWidth = 0.025f;
+    private float arcChangeAngle = 0.2f;
 
     private float degress;
     private float arcWidth;
     private float cricleWidth;
+    boolean isFirstCircle = true;
 
     public InsLoadingView(Context context) {
         super(context);
@@ -30,84 +37,70 @@ public class InsLoadingView  extends View {
         onCreateAnimators();
     }
 
-
     public InsLoadingView(Context context, AttributeSet attrs) {
         super(context, attrs);
         postInvalidate();
         onCreateAnimators();
-     }
+    }
 
     public InsLoadingView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         postInvalidate();
-     }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public InsLoadingView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-     }
+    }
 
     @Override
     protected synchronized void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (DEBUG)  {
+        if (DEBUG) {
             Log.d(TAG, "onDraw " + getWidth() + "---" + getHeight());
         }
-        drawTrack(canvas, new Paint());
+        Paint paint = getPaint(getColor(0), getColor(360) , 360);
+        drawTrack(canvas, paint);
         postInvalidate();
     }
 
     void drawTrack(Canvas canvas, Paint paint) {
-        paint.setColor(Color.BLACK);
-        canvas.rotate(degress,centerX(),centerY());
-        //canvas.drawArc(rectF,-60,30,false,paint);
-        canvas.rotate(12,centerX(),centerY());
-        RectF rectF=new RectF((float) (getWidth()*0.2),(float) (getWidth()*0.2),(float) (getWidth()*0.8), (float)(getHeight()*0.8));
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth((float)(getHeight()*0.02));
-        //canvas.drawArc(rectF,0,cricleWidth,false,paint);
-        if (DEBUG)  {
+        canvas.rotate(degress, centerX(), centerY());
+        canvas.rotate(12, centerX(), centerY());
+        RectF rectF = new RectF((float) (getWidth() * (1 - circleDia)), (float) (getWidth() * (1 - circleDia)),
+                (float) (getWidth() * circleDia), (float) (getHeight() * circleDia));
+        if (DEBUG) {
             Log.d(TAG, "cricleWidth:" + cricleWidth);
         }
         if (cricleWidth < 0) {
-            canvas.drawArc(rectF,cricleWidth,0-cricleWidth,false,paint);
+            //a
+            float startArg = cricleWidth+ 360;
+            canvas.drawArc(rectF, startArg, 360 - startArg, false, paint);
             float adjustCricleWidth = cricleWidth + 360;
             float width = 8;
-            while (adjustCricleWidth > 0) {
-                width = width - 0.2f;
-                adjustCricleWidth = adjustCricleWidth -12;
-                canvas.drawArc(rectF,adjustCricleWidth ,width ,false,paint);
+            while (adjustCricleWidth > 12) {
+                width = width - arcChangeAngle;
+                adjustCricleWidth = adjustCricleWidth - 12;
+                canvas.drawArc(rectF, adjustCricleWidth, width, false, paint);
             }
         } else {
-            for (int i = 0; i <= 4 ; i ++) {
-                if (12*i > cricleWidth) {
+            //b
+            for (int i = 0; i <= 4; i++) {
+                if (12 * i > cricleWidth) {
                     break;
                 }
-                canvas.drawArc(rectF,cricleWidth - 12*i,8 + i,false,paint);
+                canvas.drawArc(rectF, cricleWidth - 12 * i, 8 + i, false, paint);
             }
-            if (cricleWidth > 48 ) {
-                canvas.drawArc(rectF,0,cricleWidth - 48,false,paint);
+            if (cricleWidth > 48) {
+                canvas.drawArc(rectF, 0, cricleWidth - 48, false, paint);
             }
             float adjustCricleWidth = 360;
-            float width = 8 * (360 - cricleWidth)/360;
-            if (DEBUG)  {
+            float width = 8 * (360 - cricleWidth) / 360;
+            if (DEBUG) {
                 Log.d(TAG, "width:" + width);
             }
-            while (width > 0) {
-                width = width - 0.2f;
-                adjustCricleWidth = adjustCricleWidth  - 12;
-                canvas.drawArc(rectF, adjustCricleWidth ,width ,false,paint);
+            while (width > 0 && adjustCricleWidth > 12) {
+                width = width - arcChangeAngle;
+                adjustCricleWidth = adjustCricleWidth - 12;
+                Log.d(TAG, "adjustCricleWidth:" + adjustCricleWidth);
+                canvas.drawArc(rectF, adjustCricleWidth, width, false, paint);
             }
         }
-
-/*        for (int i = 0 ; i < 30 ; i ++) {
-            canvas.rotate(12,centerX(),centerY());
-             paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.BLUE);
-            paint.setStrokeWidth((float)(getHeight()*0.02));
-            canvas.drawArc(rectF,0,arcWidth,false,paint);
-        }*/
-
     }
 
     @Override
@@ -119,20 +112,20 @@ public class InsLoadingView  extends View {
     }
 
     protected float centerX() {
-        return getWidth()/2;
+        return getWidth() / 2;
     }
 
     protected float centerY() {
-        return getHeight()/2;
+        return getHeight() / 2;
     }
 
     public ArrayList<ValueAnimator> onCreateAnimators() {
-        ArrayList<ValueAnimator> animators=new ArrayList<>();
-        ValueAnimator rotateAnim= ValueAnimator.ofFloat(0,180,360);
+        ArrayList<ValueAnimator> animators = new ArrayList<>();
+        ValueAnimator rotateAnim = ValueAnimator.ofFloat(0, 180, 360);
         rotateAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                degress= (float) animation.getAnimatedValue();
+                degress = (float) animation.getAnimatedValue();
                 postInvalidate();
             }
         });
@@ -140,27 +133,42 @@ public class InsLoadingView  extends View {
         rotateAnim.setDuration(10000);
         rotateAnim.setRepeatCount(-1);
         animators.add(rotateAnim);
-        ValueAnimator arcAnim= ValueAnimator.ofFloat(12,0);
-        arcAnim.setInterpolator(new LinearInterpolator());
-        arcAnim.setDuration(1500);
-        arcAnim.setRepeatCount(-1);
-        arcAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                arcWidth= (float) animation.getAnimatedValue();
-                postInvalidate();
-            }
-        });
-        animators.add(arcAnim);
-        ValueAnimator circleDAnimator= ValueAnimator.ofFloat(-360,360);
-        circleDAnimator.setInterpolator(new LinearInterpolator());
-        circleDAnimator.setDuration(12000);
+        ValueAnimator arcAnim = ValueAnimator.ofFloat(12, 0);
+        ValueAnimator circleDAnimator = ValueAnimator.ofFloat(0, 360);
+        circleDAnimator.setInterpolator(new DecelerateInterpolator());
+        circleDAnimator.setDuration(3000);
         circleDAnimator.setRepeatCount(-1);
         circleDAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                cricleWidth = (float) animation.getAnimatedValue();
+                if (isFirstCircle) {
+                    cricleWidth = (float) animation.getAnimatedValue();
+                } else {
+                    cricleWidth = (float) animation.getAnimatedValue() - 360;
+                }
+                Log.d(TAG, "cricleWidth: " + cricleWidth + " isFirstCircle:" + isFirstCircle);
                 postInvalidate();
+            }
+        });
+        circleDAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                isFirstCircle = !isFirstCircle;
             }
         });
 
@@ -170,10 +178,33 @@ public class InsLoadingView  extends View {
         return animators;
     }
 
-    public class DeceAcceInterpolator implements Interpolator {
-        @Override
-        public float getInterpolation(float input) {
-            return ((4*input-2)*(4*input-2)*(4*input-2))/16f + 0.5f;
+    private static int getColor(double degree) {
+        if (degree < 0 || degree > 360) {
+            Log.d(TAG, "getColor error:" + degree);
         }
+        int startColor = Color.YELLOW;
+        int endColor = Color.RED;
+        double radio = degree/360;
+        int redStart = Color.red(startColor);
+        int blueStart = Color.blue(startColor);
+        int greenStart = Color.green(startColor);
+        int redEnd = Color.red(endColor);
+        int blueEnd = Color.blue(endColor);
+        int greenEnd = Color.green(endColor);
+        int red = (int) (redStart + ((redEnd - redStart) * radio + 0.5));
+        int greed = (int) (greenStart + ((greenEnd - greenStart) * radio + 0.5));
+        int blue = (int) (blueStart + ((blueEnd - blueStart) * radio + 0.5));
+        return Color.argb(255,red, greed, blue);
+    }
+
+    private Paint getPaint(int startColor, int endColor, double arcWidth) {
+        Paint paint = new Paint();
+        Shader shader = new LinearGradient( 0f,  0f,  (float)(getWidth() * circleDia*(arcWidth - 48)/360),
+                getHeight() * strokeWidth, startColor, endColor, CLAMP);
+        //paint = new Paint();
+        paint.setShader(shader);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(getHeight() * strokeWidth);
+        return paint;
     }
 }
