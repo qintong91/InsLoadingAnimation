a06ce73ead77a35f4185156b783bc001aa808bb4
Ronak Doshi <ronakdoshi10@gmail.com>
2019-05-22 15:53:50 +0530

    Add clicked color and progress corner type attribute.

--- app/src/main/java/com/example/qintong/animatordemo/DemoActivity.kt
+++ app/src/main/java/com/example/qintong/animatordemo/DemoActivity.kt
@@ -1,6 +1,5 @@
 package com.example.qintong.animatordemo
 
-import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
@@ -13,20 +12,20 @@ class DemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loading_view?.setCircleDuration(2000)
+        /*loading_view?.setCircleDuration(2000)
         loading_view?.setRotateDuration(10000)
         loading_view?.setStartColor(Color.YELLOW)
-        loading_view?.setEndColor(Color.BLUE)
+        loading_view?.setEndColor(Color.BLUE)*/
         loading_view?.setOnClickListener {
             when (loading_view?.status) {
                 InsLoadingView.Status.UNCLICKED -> loading_view?.status = InsLoadingView.Status.LOADING
                 InsLoadingView.Status.LOADING -> loading_view?.status = InsLoadingView.Status.CLICKED
                 InsLoadingView.Status.CLICKED -> loading_view?.status = InsLoadingView.Status.UNCLICKED
             }
-            Toast.makeText(this@DemoActivity, "click !", LENGTH_SHORT).show()
+            Toast.makeText(this@DemoActivity, "Click!", LENGTH_SHORT).show()
         }
         loading_view?.setOnLongClickListener {
-            Toast.makeText(this@DemoActivity, "long click !", LENGTH_SHORT).show()
+            Toast.makeText(this@DemoActivity, "Long Click!", LENGTH_SHORT).show()
             true
         }
     }
